8d1ee10e3487de15e28596860bf37a070b6238b1
leguter <kolalevcenko80@gmail.com>
2024-09-10 18:19:27 -1200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import "./App.css";
 // import { fetchContacts } from "./redux/contacts/contactsOps";
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import { useDispatch, useSelector } from "react-redux";
 import { Route,Routes } from "react-router-dom";
 import { selectError, selectIsLoading } from "./redux/contacts/selectors";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import ContactsPage from "./pages/ContactsPage/ContactsPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
+const RegisterPage = lazy(() => import("./pages/RegisterPage/RegisterPage"));
+const ContactsPage =   lazy(() => import("./pages/ContactsPage/ContactsPage"));
+const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+import Loader from './components/Loader/Loader'
 import { apiRefreshUser } from "./redux/auth/operations";
 import { selectIsRefreshing } from "./redux/auth/selectors";
-import HomePage from "./pages/HomePage/HomePage";
 import { RestrictedRoute } from './components/RestrictedRoute/RestrictedRoute'
-// import { PrivateRoute } from "./components/PrivateRoute/PrivateRoute";
+import { PrivateRoute } from "./components/PrivateRoute/PrivateRoute";
 function App() {
    const dispatch = useDispatch();
    const isLoading = useSelector(selectIsLoading);
@@ -23,22 +24,26 @@ useEffect(() => {
 }, [dispatch]);
   if(isRefreshing) return <p>User is refreshing, please wait</p>
   return (
-    
     <div>
-<Navigation />
-      {isLoading && !error && <b>Request in progress...</b>}
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route
-          path="/register"
-          element={<RestrictedRoute component={<RegisterPage />} />}
-        />
-        <Route
-          path="/login"
-          element={<RestrictedRoute component={<LoginPage />} />}
-        />
-        <Route path="/contacts" element={<ContactsPage />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Navigation />
+        {isLoading && !error && <b>Request in progress...</b>}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/register"
+            element={<RestrictedRoute component={<RegisterPage />} />}
+          />
+          <Route
+            path="/login"
+            element={<RestrictedRoute component={<LoginPage />} />}
+          />
+          <Route
+            path="/contacts"
+            element={<PrivateRoute component={<ContactsPage />} />}
+          />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
